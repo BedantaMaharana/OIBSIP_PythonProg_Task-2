f111f86c2a854255d008bf34d7817b46faf399e0
BedantaMaharana <bedantamaharana20@gmail.com>
2025-07-01 00:51:12 +0530

    Updated the database.py to create a csv file automatically

--- utils/database.py
+++ utils/database.py
@@ -3,6 +3,10 @@ import os
 from datetime import datetime
 
 def save_data(name, weight, height, bmi, category, file_path):
+    folder = os.path.dirname(file_path)
+    if not os.path.exists(folder):
+        os.makedirs(folder)  # ✅ Automatically create the 'assets' folder
+
     if not os.path.exists(file_path):
         pd.DataFrame(columns=["Name", "Weight", "Height", "BMI", "Category", "Date"]).to_csv(file_path, index=False)
 
